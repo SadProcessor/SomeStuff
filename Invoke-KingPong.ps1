97a0efdceef4002a2382960192f2c4b702aec98b
SadProcessor <SadProcessor@users.noreply.github.com>
2017-09-10 15:19:50 +0200

    Update Invoke-KingPong.ps1

--- Invoke-KingPong.ps1
+++ Invoke-KingPong.ps1
@@ -2,7 +2,7 @@
 .Synopsis
    King Pong - The Ping Kong...
 .DESCRIPTION
-   Power Ping. Multi-NIC. Auto Subnet. Multi-Thread.
+   Power Ping: Multi-NIC/Auto-Subnet/Multi-Thread.
    Returns only IPs where Success.
    Inspired by a tweet from @Lee_Holmes.
 .EXAMPLE
@@ -17,9 +17,9 @@ Function Invoke-KingPong{
     Begin{
         ## Subs
         # IP to Num
-        function IP2NUM($IP){return [int64]([int64]$IP.split(".")[0]*16777216+[int64]$IP.split(".")[1]*65536+[int64]$IP.split(".")[2]*256+[int64]$IP.split(".")[3])} 
+        function IP2NUM{Param([String]$IP); return [int64]([int64]$IP.split(".")[0]*16777216+[int64]$IP.split(".")[1]*65536+[int64]$IP.split(".")[2]*256+[int64]$IP.split(".")[3])} 
         # Num to IP
-        function NUM2IP([int64]$Num){return (([math]::truncate($Num/16777216)).tostring()+"."+([math]::truncate(($Num%16777216)/65536)).tostring()+"."+([math]::truncate(($Num%65536)/256)).tostring()+"."+([math]::truncate($Num%256)).tostring())}
+        function NUM2IP{Param([Int64]$Num); return (([math]::truncate($Num/16777216)).tostring()+"."+([math]::truncate(($Num%16777216)/65536)).tostring()+"."+([math]::truncate(($Num%65536)/256)).tostring()+"."+([math]::truncate($Num%256)).tostring())}
         ## Prep
         # Prep empty IP list
         $IPList = @()
@@ -37,17 +37,17 @@ Function Invoke-KingPong{
             $Brdcst = New-Object Net.IPAddress ([Net.IPAddress]::parse("255.255.255.255").address -bxor $Mask.address -bor $Ntwrk.address)
             Write-Verbose "$($Ntwrk.IPAddressToString) / $Mask" 
             # Start & End
-            $Start  = (IP2NUM($ntwrk.ipaddresstostring))  +1
-            $End    = (IP2NUM($brdcst.ipaddresstostring)) -1
+            $Start = (IP2NUM -IP $ntwrk.ipaddresstostring)  +1
+            $End   = (IP2NUM -IP $brdcst.ipaddresstostring) -1
             # Add IPs to IPList
-            For($n=$Start; $n -le $End; $n++){$IPList += NUM2IP($n)}
+            For($n=$Start; $n -le $End; $n++){$IPList += NUM2IP -Num $n}
             }
         # Ping IP List
         $Ping = $IPList | sort -Unique | %{(New-Object Net.NetworkInformation.Ping).SendPingAsync($_,250)}
         [Threading.Tasks.Task]::WaitAll($Ping) 
         # Get IPs where Success
-        $Result = ($Ping.Result | ? Status -eq Success | Select -ExpandProp address).IPAddressToString
-        Write-Verbose "Success: $($Result.count) / $($Ping.Result.count)"
+        $Success = ($Ping.Result | ? Status -eq Success | Select -ExpandProp address).IPAddressToString
+        Write-Verbose "Success: $($Success.count) / $($Ping.Result.count)"
         }
-    End{return $Result}
+    End{return $Success}
     } 
